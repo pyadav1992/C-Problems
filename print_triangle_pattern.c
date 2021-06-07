69ee77ce455fccedcdb3867570b340d1ef3cb864
Pratik Yadav <45407020+pyadav1992@users.noreply.github.com>
2021-06-07 10:43:14 -0700

    Create print_triangle_pattern.c

---
+++ print_triangle_pattern.c
@@ -0,0 +1,34 @@
+// { Driver Code Starts
+//Initial Template for C
+
+#include <stdio.h>
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+void triangleWall(int s){
+    
+    int row, col;
+    //Write your code here
+    for (row=0; row<s; row++)
+    {
+        for (col=0; col<=row; col++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
+
+// { Driver Code Starts.
+    
+int main() {
+	
+	int t; scanf("%d", &t);
+	
+	while(t--){
+	    int s; scanf("%d", &s);
+        triangleWall(s);
+	}
+}  // } Driver Code Ends
